256064edc5380e7af5bd19fded33240b0887e071
Cain.Chu <cainmaila@gmail.com>
2015-12-28 11:37:37 +0800

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function (message_) {
+    return {
+        print:function () {
+            console.log(message_);
+        }
+    };
+};
--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-# npm_pushlish_demo
-npm pushlish demo
-
-```javascriot
 var cain_message = require('./index.js')("message !!");
 cain_message.print();
-```
